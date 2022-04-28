a96b661f35d27469297388ae8d9597d8140b7594
MatthiasHeilManchester <M.Heil@maths.manchester.ac.uk>
2022-04-28 13:06:01 +0100

    initial hello world

---
+++ ewa_melt_crystallise_anneal.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<cstdlib>
+
+
+//------------------------------------------------------------
+// Driver
+//------------------------------------------------------------
+main()
+{
+
+ // Are we really done?
+ std::cout << "Done ";
+}
